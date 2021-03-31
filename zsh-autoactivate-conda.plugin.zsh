b41e6be3ea8700665efaee6502680f1b1f6ca3ea
Kevin Murray <foss@kdmurray.id.au>
2021-03-31 19:24:31 +1100

    consistent naming

--- zsh-autoactivate-conda.plugin.zsh
+++ zsh-autoactivate-conda.plugin.zsh
@@ -1,15 +1,13 @@
 autoload -U add-zsh-hook
-condadir() {
+zsh_autoactivate_conda() {
     if [[ -f environment.yml && -r environment.yml && -n "$(which conda 2>/dev/null)" ]];
     then
-        echo "in autoconda"
         env=$(python3 -c 'import yaml; print(yaml.load(open("environment.yml"))["name"])' 2>/dev/null)
         if [ -n "$env" ]
         then
-            echo conda activate "$env"
             conda activate "$env" 2>/dev/null
         fi
     fi
 }
-add-zsh-hook chpwd condadir
+add-zsh-hook chpwd zsh_autoactivate_conda
 
