6b7a493dde041f58cff045a7569947ae39895d12
Dr. K. D. Murray <1560490+kdm9@users.noreply.github.com>
2022-06-16 04:14:02 +1000

    Fix deprecation from PyYAML > 6

--- zsh-autoactivate-conda.plugin.zsh
+++ zsh-autoactivate-conda.plugin.zsh
@@ -1,11 +1,12 @@
 autoload -U add-zsh-hook
+ZSH_AUTOACTIVATE_CONDA_BINNAME="${ZSH_AUTOACTIVATE_CONDA_BINNAME:-conda}"
 zsh_autoactivate_conda() {
-    if [[ -f environment.yml && -r environment.yml && -n "$(which conda 2>/dev/null)" ]];
+    if [[ -f environment.yml && -r environment.yml && -n "$(which "${ZSH_AUTOACTIVATE_CONDA_BINNAME}" 2>/dev/null)" ]];
     then
-        env=$(python3 -c 'import yaml; print(yaml.load(open("environment.yml"))["name"])' 2>/dev/null)
+        env=$(python3 -c 'import yaml; print(yaml.safe_load(open("environment.yml"))["name"])' 2>/dev/null)
         if [ -n "$env" ]
         then
-            conda activate "$env" 2>/dev/null
+            "${ZSH_AUTOACTIVATE_CONDA_BINNAME}" activate "$env" 2>/dev/null
         fi
     fi
 }
